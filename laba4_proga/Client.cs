f3d681f3a273e26fe0fa31fbaa8111476e90ce2d
alino4kaaaa <alina101104@mail.ru>
2024-05-13 18:39:34 +0700

    Добавьте файлы проекта.

---
+++ laba4_proga/Client.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace laba4_proga
+{
+    internal class Client
+    {
+        public TcpClient topClient;
+        public NetworkStream stream;
+
+        public Client(string adress, int port)
+        {
+            topClient = new TcpClient(adress, port);
+            stream = topClient.GetStream();
+        }
+
+        public string Request(string question)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(question);
+
+            stream.Write(bytes, 0, bytes.Length);
+
+            byte[] bytes1 = new byte[256];
+
+            stream.Read(bytes1, 0, bytes1.Length);
+
+            string answer = Encoding.UTF8.GetString(bytes1);
+
+            return answer;
+        }
+    }
+}
---
+++ laba4_proga/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba4_proga
+{
+    public partial class Form1 : Form
+    {
+        private Client client;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.client = new Client(maskedTextBox1.Text, int.Parse(maskedTextBox2.Text));
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                listBox1.Items.Add(drive.Name);
+            }
+            DirectoryInfo directoryInfo = new DirectoryInfo(drives[0].Name);
+            DirectoryInfo[] dirs = directoryInfo.GetDirectories();
+            foreach(DirectoryInfo dir in dirs)
+            {
+                listBox2.Items.Add(dir.Name);
+            }
+            button1.Enabled = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string[] answers = this.client.Request(listBox2.SelectedItem.ToString()).Split('#');
+                foreach (string answer in answers)
+                {
+                    listBox3.Items.Add(answer);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Каталог не доступен.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.client.stream.Close();
+            this.client.topClient.Close();
+            this.Close();
+        }
+    }
+}
